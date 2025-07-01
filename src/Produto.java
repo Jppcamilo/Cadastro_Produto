4c9348d0522fa187f30cf190d427a8e693019f58
Jppcamilo <jpcamilopereira@gmail.com>
2025-07-01 14:59:58 -0300

    Adicionei os atributos, o contrutor e o metodo na classe Produto.java

---
+++ src/Produto.java
@@ -0,0 +1,19 @@
+public class Produto {
+
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto (String nome, double preco, int quantidade){
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String mostrar() {
+        return "Nome: " + nome + "\n" +
+               "Preço: R$ " + preco + "\n" +
+               "Quantidade: " + quantidade + "\n";
+    }
+
+}
