ea67880d6c1082f3f7a44a35c3333b609b293e8e
Jppcamilo <jpcamilopereira@gmail.com>
2025-07-01 15:08:33 -0300

    Arquivos universais

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitordeDados = new Scanner(System.in);
+        ArrayList<Produto> listadeProduto = new ArrayList<>();
+
+        System.out.println("+----------------------+-------------------------+");
+        System.out.println("| Opção               | Descrição               |");
+        System.out.println("+----------------------+-------------------------+");
+        System.out.println("| 1                   | Listar produtos         |");
+        System.out.println("| 2                   | Adicionar produto       |");
+        System.out.println("| 3                   | Remover produto         |");
+        System.out.println("| 4                   | Sair                    |");
+        System.out.println("+----------------------+-------------------------+");
+        int opcao = leitordeDados.nextInt();
+
+        while (opcao != 4){
+
+            switch ()
+
+        }
+
+
+    }
+}
