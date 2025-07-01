f7b5f86d4ef8c7356c068758449a80920e23d3cb
Jppcamilo <jpcamilopereira@gmail.com>
2025-07-01 15:26:41 -0300

    Criei o caso lista de produtos para mostrar para o usuario

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,44 @@ public class Main {
         Scanner leitordeDados = new Scanner(System.in);
         ArrayList<Produto> listadeProduto = new ArrayList<>();
 
-        System.out.println("+----------------------+-------------------------+");
-        System.out.println("| Opção               | Descrição               |");
-        System.out.println("+----------------------+-------------------------+");
-        System.out.println("| 1                   | Listar produtos         |");
-        System.out.println("| 2                   | Adicionar produto       |");
-        System.out.println("| 3                   | Remover produto         |");
-        System.out.println("| 4                   | Sair                    |");
-        System.out.println("+----------------------+-------------------------+");
-        int opcao = leitordeDados.nextInt();
+        int opcao = 0;
 
-        while (opcao != 4){
+        while (opcao != 4) {
+            System.out.println("+----------------------+-------------------------+");
+            System.out.println("| Opção               | Descrição               |");
+            System.out.println("+----------------------+-------------------------+");
+            System.out.println("| 1                   | Lista produtos         |");
+            System.out.println("| 2                   | Adicionar produto       |");
+            System.out.println("| 3                   | Remover produto         |");
+            System.out.println("| 4                   | Sair                    |");
+            System.out.println("+----------------------+-------------------------+");
+            opcao = leitordeDados.nextInt();
 
-            switch ()
+            switch (opcao) {
+
+                case 1:
+                    if (listadeProduto.isEmpty()) {
+                        System.out.println("Nenhum produto cadastrado.");
+                        System.out.println("Deseja adicionar um produto? (S/N)");
+                        String opcaoSimNao = leitordeDados.next();
+                        if (opcaoSimNao.equalsIgnoreCase("S")) {
+                            opcao = 2;
+                        }
+                    } else {
+                        System.out.println("Lista de produtos:");
+                        for (Produto produto : listadeProduto) {
+                            System.out.println(produto.mostrar());
+                        }
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Teste");
+
+            }
 
         }
-
-
+        leitordeDados.close();
     }
 }
+
